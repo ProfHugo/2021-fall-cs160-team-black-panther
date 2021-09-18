d54d1d4e1558b27291042cde442acd742fb89b0c
luksaweephansri <luksawee.phansri@gmail.com>
2021-09-17 18:35:16 -0700

    test commit

--- client/app/src/main/java/net/tbp/interval/MainActivity.java
+++ client/app/src/main/java/net/tbp/interval/MainActivity.java
@@ -33,6 +33,5 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-    // Test
 
 }
