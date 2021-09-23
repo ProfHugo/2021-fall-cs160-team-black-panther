2cd87ebc64bfd7922a2242e64bde5631c5a02d8f
Hugo Wong <hugothepvpmaster0722@gmail.com>
2021-09-22 17:28:36 -0700

    autogenerated Spring app server
    dependencies:
    Spring Web
    MySQL Driver
    Spring Data JPA

---
+++ server/src/main/java/net/tbp/interval/IntervalApplication.java
@@ -0,0 +1,13 @@
+package net.tbp.interval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervalApplication.class, args);
+	}
+
+}
